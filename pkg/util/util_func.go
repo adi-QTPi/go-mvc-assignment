7a073acb92872dad70c080c4471a52901461dad5
karma <captainav0608@gmail.com>
2025-08-09 20:30:51 +0530

    [feature] add unti test for a function
    Signed-off-by: karma <captainav0608@gmail.com>

--- pkg/util/util_func.go
+++ pkg/util/util_func.go
@@ -131,7 +131,7 @@ func StringToSqlNullInt64(s string) (sql.NullInt64, error) {
 	var ans sql.NullInt64
 	parsedInt, err := strconv.ParseInt(s, 10, 64)
 	if err != nil {
-		return ans, err
+		return ans, fmt.Errorf("Invalid input")
 	}
 	ans.Int64 = parsedInt
 	ans.Valid = true
